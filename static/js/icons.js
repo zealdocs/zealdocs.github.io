54adac7c92a20f195dab6fe24254f59bb1288224
Oleg Shparber <trollixx@gmail.com>
2016-03-31 22:49:23 -0400

    Cleanup and optimize icons.js

--- static/js/icons.js
+++ static/js/icons.js
@@ -3,13 +3,11 @@
 $(function() {
     var docs = {
     {% for doc in site.data.docsets %}
-        '{{ doc.title }}': '{{ doc.icon }}'{% if forloop.last %}{% else %},{% endif %}
+        '{{ doc.title }}': '{{ doc.icon }}'{% unless forloop.last %},{% endunless %}
     {% endfor %}
     };
-    $('.docset').each(function() {
-        var title = $(this).find('span').text();
-        $(this).find('img').attr(
-            'src', 'data:;base64,' + docs[title]
-        );
+    [].forEach.call(document.getElementsByClassName('docset'), function(el) {
+        var title = el.querySelector('span').textContent;
+        el.querySelector('img').setAttribute('src', 'data:;base64,' + docs[title]);
     });
 })
