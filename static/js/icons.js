773c93982a1ca08ba8dcbd03e762f79fcea6f1ce
Oleg Shparber <trollixx@gmail.com>
2016-03-31 23:42:34 -0400

    index: Cleanup and optimize the docset list

--- static/js/icons.js
+++ static/js/icons.js
@@ -6,8 +6,8 @@ document.addEventListener("DOMContentLoaded", function() {
         '{{ doc.title }}': '{{ doc.icon }}'{% unless forloop.last %},{% endunless %}
     {% endfor %}
     };
-    [].forEach.call(document.getElementsByClassName('docset'), function(el) {
-        var title = el.querySelector('span').textContent;
-        el.querySelector('img').setAttribute('src', 'data:;base64,' + docs[title]);
+
+    [].forEach.call(document.getElementById('docset-list').querySelectorAll('li'), function(el) {
+        el.setAttribute('style', 'background-image: url(data:image/png;base64,' + docs[el.textContent]) + ')';
     });
 });
